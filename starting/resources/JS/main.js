11ea5f0cf6591053c603ef0c3ddf1da34360f0ab
StormTebbutt <stormrc390@gmail.com>
2023-06-07 12:01:54 -0500

    Initial upload for website

---
+++ starting/resources/JS/main.js
@@ -0,0 +1,8 @@
+let enterInfo = document.getElementById('info-form')
+let infoButton = document.getElementById('info-button')
+
+enterInfo.style.display = 'none';
+
+infoButton.onclick = () => {
+    enterInfo.style.display = 'block'
+}
